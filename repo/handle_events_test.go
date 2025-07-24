201bb9dbeec04556f93108cb9b675ef2304384b2
Alfonso <fernandez.alfonso@gmail.com>
2025-07-24 11:51:54 +0200

    Add connection retry support

--- repo/handle_events_test.go
+++ repo/handle_events_test.go
@@ -1,6 +1,10 @@
 package repo
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
+)
 
 func TestRepoHandleConnectionEvents(t *testing.T) {
 	h1 := NewRepoHandle(New())
@@ -79,6 +83,44 @@ func TestRepoHandleConnComplete(t *testing.T) {
 	// drain events
 	<-h1.Events
 	<-h1.Events
+
+	h1.Close()
+	h2.Close()
+}
+
+func TestRepoHandleReconnect(t *testing.T) {
+	h1 := NewRepoHandle(New())
+	h2 := NewRepoHandle(New())
+
+	rc := make(chan *mockConn, 2)
+	dial := func(ctx context.Context) (Conn, error) {
+		c1, c2 := newMockConn()
+		_ = h2.AddConn(h1.Repo.ID, c2)
+		rc <- c2
+		return c1, nil
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cc := h1.AddConnWithRetry(ctx, h2.Repo.ID, dial, 10*time.Millisecond)
+
+	if evt := <-h1.Events; evt.Type != EventPeerConnected || evt.Peer != h2.Repo.ID {
+		t.Fatalf("expected peer connected event, got %#v", evt)
+	}
+	first := <-rc
+	first.Close()
+
+	<-h1.Events // conn_error
+	if evt := <-h1.Events; evt.Type != EventPeerDisconnected {
+		t.Fatalf("expected peer disconnected, got %#v", evt)
+	}
+
+	if evt := <-h1.Events; evt.Type != EventPeerConnected {
+		t.Fatalf("expected peer reconnected, got %#v", evt)
+	}
+	second := <-rc
+	cancel()
+	second.Close()
+	_ = cc.Await()
 
 	h1.Close()
 	h2.Close()
